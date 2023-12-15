2003a3b1846e56838eaf6221e60253c5e01f5af7
Mr-Incognito <mrincognito1109@gmail.com>
2023-12-16 00:17:47 +0530

    242 Valid_Anagram

--- Easy/ValidAnagram.java
+++ Easy/ValidAnagram.java
@@ -4,13 +4,49 @@ public class ValidAnagram {
 
         String s = "anagram";
         String t = "nagaram";
+
+        System.out.println(new ValidAnagram().isAnagram(s, t));
         
     }
 
     public boolean isAnagram(String s, String t) {
 
-        for(int i = )
+        // Convert both the strings into lowercase 
+
+        s = s.toLowerCase();
+        t = t.toLowerCase();
+
+        // get rid of the spaces in the strings 
         
+        s = s.replace(" ", "");
+        t = t.replace(" ", "");
+
+        // initialize a bucket array 
+
+        int[] bucket = new int[26];
+        
+        // iterate through the first string 
+        
+        for (int i = 0; i < s.length(); i++) {
+            bucket[s.charAt(i) - 'a']++;
+        }
+
+        // iterate through the second string 
+
+        for (int i = 0; i < t.length(); i++) {
+            bucket[t.charAt(i) - 'a']--;
+        }
+
+        // iterate through the bucket array 
+
+        for (int i = 0; i < bucket.length; i++) {
+            if (bucket[i] != 0) {
+                return false;
+            }
+        }
+        return true;
+
+
+
     }
-    
 }
