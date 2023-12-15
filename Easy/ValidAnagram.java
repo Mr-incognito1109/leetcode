2a881cce7adfb22784d79ba2311ae68920cd7921
Mr-Incognito <mrincognito1109@gmail.com>
2023-12-16 00:00:11 +0530

    added 242 ValidAnagram

---
+++ Easy/ValidAnagram.java
@@ -0,0 +1,16 @@
+public class ValidAnagram {
+
+    public static void main(String[] args) {
+
+        String s = "anagram";
+        String t = "nagaram";
+        
+    }
+
+    public boolean isAnagram(String s, String t) {
+
+        for(int i = )
+        
+    }
+    
+}
